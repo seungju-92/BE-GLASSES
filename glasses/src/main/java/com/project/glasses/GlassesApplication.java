8e9349f41d3bbe4e3d62098277eeac9893fd97cc
leeseungju <leeseungju92@gmail.com>
2025-09-11 23:00:18 +0900

    123

---
+++ glasses/src/main/java/com/project/glasses/GlassesApplication.java
@@ -0,0 +1,13 @@
+package com.project.glasses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlassesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlassesApplication.class, args);
+    }
+
+}
